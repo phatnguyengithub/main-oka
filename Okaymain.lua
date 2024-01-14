7d8f28c1660ca6373385ccc5f10cb353a89580b2
phatnguyengithub <156379267+phatnguyengithub@users.noreply.github.com>
2024-01-14 12:55:50 +0700

    Add files via upload

---
+++ Okaymain.lua
@@ -0,0 +1,5 @@
+---// Join MTien Hub Sever To Update New Script \\--
+--// Sever Discord: https://discord.com/invite/pCBDH5z382 \\--
+_G.Name = "Okay hub" -- Put You Name Script Here
+_G.Logo = "15943864909" -- Enter Your ID Image Here
+loadstring(Game:HttpGet"https://raw.githubusercontent.com/ImTienNguyenZ/NguyenTienSimpRose/main/NguyenMinhTienLoader")()
